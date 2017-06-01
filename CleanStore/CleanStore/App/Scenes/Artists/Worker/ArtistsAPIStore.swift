618a42547489066daff42032e82c77a0d5d7561b
hoi <hoi@co8xiss-MacBook-Pro.local>
2017-06-01 15:30:34 +0700

    artists scene

--- CleanStore/CleanStore/App/Scenes/Artists/Worker/ArtistsAPIStore.swift
+++ CleanStore/CleanStore/App/Scenes/Artists/Worker/ArtistsAPIStore.swift
@@ -41,6 +41,42 @@ extension ArtistsAPIStore: ArtistsStoreProtocol {
     /// - parameter completion: The completion block
     func fetchArtists(completion: @escaping ([Artist]?, Error?) -> ()) {
         let limit = Constants.topArtistsLimit
-        guard let url = 
+        guard let url = LastFMAPIEndPoint.getTopArtists(limit).url() else {
+            completion([], ArtistsStoreError.invalidURL)
+            return
+        }
+        
+        let request = URLRequest.jsonRequest(url: url)
+        
+        networkClient.sendRequest(request: request) { data, response, error in
+            
+            var artists: [Artist]?
+            var artistsError: Error?
+            
+            if let json = data?.jsonDictionary() {
+                
+                if let artistsDictionary = json[Constants.topArtistsDictionaryKey] as? [String: Any],
+                let artistsArray = artistsDictionary[Constants.topArtistsArrayKey] as? [[String: Any]] {
+                    
+                    artists = artistsArray.flatMap { artistsDictionary -> Artist? in
+                        
+                        return Artist.fromJSON(json: artistsDictionary)
+                    }
+                    
+                } else {
+                
+                    artistsError = ArtistsStoreError.invalidResponse
+                    
+                }
+                
+            } else {
+                
+                artistsError = ArtistsStoreError.invalidResponse
+                
+            }
+            
+            completion(artists, artistsError)
+            
+        }
     }
 }
