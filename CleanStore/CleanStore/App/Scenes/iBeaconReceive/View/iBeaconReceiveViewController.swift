e45e4d640d1cbfd969c8c6c0ff838e07ee154915
dangquochoi2007 <dangquochoi2007@gmail.com>
2017-06-12 07:27:58 +0700

    add scene beacon receive

--- CleanStore/CleanStore/App/Scenes/iBeaconReceive/View/iBeaconReceiveViewController.swift
+++ CleanStore/CleanStore/App/Scenes/iBeaconReceive/View/iBeaconReceiveViewController.swift
@@ -5,66 +5,60 @@
 //  Created by QuocHoi on 9/6/17.
 //  Copyright (c) 2017 hoi. All rights reserved.
 //
-
 import UIKit
 import CoreLocation
 
 protocol iBeaconReceiveViewControllerInput: iBeaconReceivePresenterOutput {
-
+    
 }
 
 protocol iBeaconReceiveViewControllerOutput {
-
+    
     func doSomething()
 }
 
 final class iBeaconReceiveViewController: UIViewController {
-
+    
     var output: iBeaconReceiveViewControllerOutput!
     var router: iBeaconReceiveRouterProtocol!
-
-
+    
+    
     // MARK: - Initializers
-
     init(configurator: iBeaconReceiveConfigurator = iBeaconReceiveConfigurator.sharedInstance) {
-
+        
         super.init(nibName: "iBeaconReceiveViewController", bundle: nil)
-
+        
         configure()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
-
+        
         super.init(coder: aDecoder)
-
+        
         configure()
     }
-
-
+    
+    
     // MARK: - Configurator
-
     private func configure(configurator: iBeaconReceiveConfigurator = iBeaconReceiveConfigurator.sharedInstance) {
-
+        
         configurator.configure(viewController: self)
     }
-
-
+    
+    
     // MARK: - View lifecycle
-
     override func viewDidLoad() {
-
+        
         super.viewDidLoad()
-
+        
         doSomethingOnLoad()
     }
-
-
+    
+    
     // MARK: - Load data
-
     func doSomethingOnLoad() {
-
+        
         // TODO: Ask the Interactor to do some work
-
         output.doSomething()
     }
 }
@@ -81,14 +75,12 @@ extension iBeaconReceiveViewController: CLLocationManagerDelegate {
 
 
 // MARK: - iBeaconReceivePresenterOutput
-
 extension iBeaconReceiveViewController: iBeaconReceiveViewControllerInput {
-
-
+    
+    
     // MARK: - Display logic
-
     func displaySomething(viewModel: iBeaconReceiveViewModel) {
-
+        
         // TODO: Update UI
     }
 }
