12a5c4215829e30a2496f860b7b3e77060aa8768
hoi <hoi@co8xiss-MacBook-Pro.local>
2017-07-04 09:34:04 +0700

    add profile setting scene

---
+++ CleanStore/CleanStore/App/Scenes/ProfileSettings/View/UIView/ProfileSettingsHeaderView.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileSettingsHeaderView.swift
+//  CleanStore
+//
+//  Created by hoi on 4/7/17.
+//  Copyright © 2017 hoi. All rights reserved.
+//
+
+import Foundation
---
+++ CleanStore/CleanStore/App/Scenes/ProfileSettings/View/UIView/ProfileSettingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileSettingsTableViewCell.swift
+//  CleanStore
+//
+//  Created by hoi on 4/7/17.
+//  Copyright © 2017 hoi. All rights reserved.
+//
+
+import UIKit
+
+class ProfileSettingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
